b9cf01a9d2765622eff9a476d4ab2408636221f5
Mátyás Antal <latna.matyas@gmail.com>
2021-10-30 10:50:37 +0200

    create Toy, change Ball

---
+++ week08/week08/Abstractions/Toy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week08.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            this.AutoSize = false;
+            this.Width = 50;
+            this.Height = 50;
+            this.Paint += Toy_Paint;
+        }
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+        protected abstract void DrawImage(Graphics gIn);
+        public virtual void MoveToy()
+        {
+            this.Left += 1;
+        }
+    }
+}
--- week08/week08/Entities/Ball.cs
+++ week08/week08/Entities/Ball.cs
@@ -5,32 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week08.Abstractions;
 
 namespace week08.Entities
 {
-    class Ball : Label
+    class Ball : Toy
     {
-        public Ball()
-        {
-            this.AutoSize = false;
-            this.Width = 50;
-            this.Height = 50;
-            this.Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics gIn)
+        protected override void DrawImage(Graphics gIn)
         {
             gIn.FillEllipse(new SolidBrush(Color.Blue), 0, 0, this.Width, this.Height);
-        }
-
-        public void MoveBall()
-        {
-            this.Left += 1;
         }
     }
 }
